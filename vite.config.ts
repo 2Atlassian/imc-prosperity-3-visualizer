10506c1c42289b276a233d1047b8558578b69be6
Jayden Liu <liujayden12@gmail.com>
2025-04-06 16:47:32 +0100

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/imc-prosperity-3-visualizer/',
   resolve: {
     alias: {
       '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
